5e74885cefe1ce320c009e6e25ce0c39a20358a0
HyeonSoo <21port@naver.com>
2019-07-25 18:04:18 +0900

    090725|jupiter

---
+++ 01_python/notes/mypackage/math/formular.py
@@ -0,0 +1,7 @@
+pi = 3.14
+
+def my_max(a, b):
+    if a > b:
+        print(f'{a}가 더 큽니다')
+    elif a < b:
+        print(f'{b}가 더 큽니다')
---
+++ entry_point/A.py
@@ -0,0 +1,8 @@
+def func():
+    print('function A.py')
+
+if __name__ == '__main__':
+    print('A.py가 직접 실행')
+else:
+    print('A.py 가 import 되어 실행')
+
